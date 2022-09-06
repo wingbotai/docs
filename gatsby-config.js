6e83116d60de3cab9b363f5f0f24a43273be8252
Ondrej Veres <ondra.veres@gmail.com>
2022-09-06 13:06:43 +0200

    smooth-doc integration

---
+++ gatsby-config.js
@@ -0,0 +1,16 @@
+'use strict';
+module.exports = {
+    pathPrefix: '/wingbot',
+    plugins: [
+        {
+            resolve: 'smooth-doc',
+            options: {
+                name: 'Wingbot Chatbot Framework Documentation',
+                description: 'Wingbot Chatbot Framework Documentation',
+                siteUrl: 'https://wingbotai.github.io/',
+                sections: ['GETTING STARTED WITH CHATBOTS', 'GETTING STARTED WITH WINGBOT.AI', 'VARIABLES AND CONVERSATION STATE', 'MARKETING AND AUDIENCES', 'ADVANCED CONVERSATIONS', 'NLP', 'TESTING'],
+                githubRepositoryURL: 'https://github.com/wingbotai/wingbot'
+            }
+        }
+    ]
+};
